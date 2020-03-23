297eae4d4e35c821781aac6037d8ca39d4dc953c
gavidya <it18225716@my.sliit.lk>
2020-03-23 15:04:01 +0530

    MAD Submission-- Tutorial 03

---
+++ app/src/main/java/submmision/sliit/tute03/Fragment1.java
@@ -0,0 +1,20 @@
+package submmision.sliit.tute03;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
